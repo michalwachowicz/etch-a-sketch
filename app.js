7012a8cc60082c9d6117956b0a9d849309370087
Michał Wachowicz <wachowiczmichal1@gmail.com>
2021-11-12 14:17:22 +0100

    Add styles and functionality to the project

---
+++ app.js
@@ -0,0 +1,46 @@
+const screen = document.querySelector('.screen')
+const resetBtn = document.querySelector('.btn--reset')
+
+function paint(e) {
+  e.target.classList.add('screen__item--painted')
+}
+
+function reset() {
+  const value = parseInt(prompt('Enter a number between 10 and 100:', '10'))
+
+  if (isNaN(value)) {
+    alert('ERROR! The value you entered is not a number!')
+    return
+  }
+
+  if (value < 10 || value > 100) {
+    alert('ERROR! The value should be in the range 10 - 100!')
+    return
+  }
+
+  generateGrid(value)
+}
+
+function clearScreen() {
+  screen.innerHTML = ''
+}
+
+function generateGrid(size) {
+  clearScreen()
+
+  for (let i = 0; i < size ** 2; i++) {
+    const item = document.createElement('div')
+
+    item.className = 'screen__item'
+    item.addEventListener('mouseover', paint)
+    item.addEventListener('touchstart', paint)
+
+    screen.appendChild(item)
+  }
+
+  screen.className = `screen--s--${size}`
+  screen.classList.add('screen')
+}
+
+resetBtn.addEventListener('click', reset)
+generateGrid(16)
